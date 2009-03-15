2fb6a033e90c6873ffc46f7caffdb3b77e5b9e2d
Daniel DeLeo <danielsdeleo@mac.com>
2009-03-14 21:17:50 -0600

    Improve duck type-ability; !TATFT!, and refactor for clarity -Changed NaiveBayes @features to @features_count; -Changed type of that variable to Hash.  Previously, each feature was converted to an integer and @features was used like a hash with integer only keys, but this breaks duck typing and requires use of the feature selector even when you ``do not want,'' as they say on the tubes. -Renamed a bunch of methods and shuffled stuff around for code clarity.  APIs used externally seem fine; can still use the example.rb provided as well as my own (not included) example. -TATFT. look it up.

--- lib/basset/naive_bayes.rb
+++ lib/basset/naive_bayes.rb
@@ -7,37 +7,42 @@ module Basset
   # it can be used to classify new documents.
   class NaiveBayes
     include YamlSerialization
+    
+    attr_reader :total_docs, :total_docs_in_class, :feature_counts
 
     def initialize
-      @number_of_documents = 0
-      @number_of_documents_in_class = Hash.new(0)
-      @features = []
-      reset_cached_probabilities
+      @total_docs = 0
+      @total_docs_in_class = Hash.new(0)
+      @feature_counts = {}
+      @occurences_of_every_feature_in_class = {}
     end
   
     # takes a classification which can be a string and
-    # a vector of numbered features.
+    # a vector of features.
     def add_document(classification, feature_vector)
       reset_cached_probabilities
     
-      @number_of_documents_in_class[classification] += 1
-      @number_of_documents += 1
+      @total_docs_in_class[classification] += 1
+      @total_docs += 1
       
       feature_vector.each do |feature|
-        @features[feature.name] ||= FeatureCount.new(feature.name)
-        @features[feature.name].add_count_for_class(feature.value, classification)
+        @feature_counts[feature.name] ||= FeatureCount.new(feature.name)
+        @feature_counts[feature.name].add_count_for_class(feature.value, classification)
       end
+    end
+    
+    def classes
+      @total_docs_in_class.keys
     end
   
     # returns the most likely class given a vector of features
-    def classify(feature_vector)
+    def classify(feature_vectors, opts={:normalize_classes=>true})
       class_probabilities = []
       
-      @number_of_documents_in_class.keys.each do |classification|
-        class_probability = Math.log10(probability_of_class(classification))
-        feature_vector.each do |feature|
-          class_probability += Math.log10(probability_of_feature_given_class(feature.name, classification)) * feature.value
-        end
+      classes.each do |classification|
+        class_probability = 0
+        class_probability += Math.log10(probability_of_class(classification)) if opts[:normalize_classes]
+        class_probability += probability_of_vectors_for_class(feature_vectors, classification)
         class_probabilities << [class_probability, classification]
       end
       
@@ -49,46 +54,68 @@ module Basset
       end
       max
     end
-  
+    
+    #
+    # Gives a score for probability of _feature_vector_ being in 
+    # class _classification_.  
+    # 
+    # This score can be normalized to the number of feature vectors by passing
+    # :normalize => true for the third argument.
+    #
+    # Score is not normalized for the relatives probabilities of each class. 
+    def probability_of_vectors_for_class(feature_vectors, classification, opts={:normalize=>false})
+      probability = 0
+      feature_vectors.each do |feature_vector|
+        probability += probability_of_vector_for_class(feature_vector, classification)
+      end
+      if opts[:normalize]
+        probability / feature_vectors.count.to_f
+      else
+        probability
+      end
+    end
+    
+    # returns the probability of a feature given the class
+    def probability_of_vector_for_class(feature_vector, classification)
+      # the reason the rescue 0 is in there is tricky
+      # because of the removal of redundant unigrams, it's possible that one of the features is never used/initialized
+      decimal_probability = (((@feature_counts[feature_vector.name].count_for_class(classification) rescue 0) + 0.1)/ occurrences_of_all_features_in_class(classification).to_f) * feature_vector.value
+      Math.log10(decimal_probability)
+    end
+    
+    # The number of times every feature occurs for a given class.
+    def occurrences_of_all_features_in_class(classification)
+      # return the cached value, if there is one
+      #return @occurences_of_every_feature_in_class[classification] if @occurences_of_every_feature_in_class[classification]
+
+      @feature_counts.each_value do |feature_count|
+        @occurences_of_every_feature_in_class[classification] ||= 0
+        @occurences_of_every_feature_in_class[classification] += feature_count.count_for_class(classification)
+      end
+
+      @occurences_of_every_feature_in_class[classification]
+    end
+
   private
   
     # probabilities are cached when the classification is run. This method resets
     # the cached probabities.
     def reset_cached_probabilities
-      @occurences_of_every_feature_in_class = Hash.new
-    end
-
-    # The number of times every feature occurs for a given class.
-    def number_of_occurences_of_every_feature_in_class(classification)
-      # return the cached value, if there is one
-      return @occurences_of_every_feature_in_class[classification] if @occurences_of_every_feature_in_class[classification]
-      
-      # we drop the first (since there is no 0 feature) and sum on the rest
-      # the reason the rescue 0 is in there is tricky
-      # because of the removal of redundant unigrams, it's possible that one of the features is never used/initialized
-      puts "@features: " + @features.inspect
-      @occurences_of_every_feature_in_class[classification] = @features.rest.compact.inject(0) do |sum, feature_count|
-        sum + feature_count.count_for_class(classification)
-      end
+      @occurences_of_every_feature_in_class.clear
     end
 
     # returns the probability of a given class
     def probability_of_class(classification)
-      @number_of_documents_in_class[classification] / @number_of_documents.to_f
-    end
-
-    # returns the probability of a feature given the class
-    def probability_of_feature_given_class(feature, classification)
-      # the reason the rescue 0 is in there is tricky
-      # because of the removal of redundant unigrams, it's possible that one of the features is never used/initialized
-      ((@features[feature].count_for_class(classification) rescue 0) + 1)/ number_of_occurences_of_every_feature_in_class(classification).to_f
+      @total_docs_in_class[classification] / @total_docs.to_f
     end
   
     # A class to store feature counts
     class FeatureCount
+      attr_reader :classes, :name
       
-      def initialize(feature_name=nil)
-        @feature_name, @classes = feature_name, {}
+      def initialize(feature_name=nil, classification=nil, count=0)
+        @name, @classes = feature_name, {}
+        add_count_for_class(count, classification) if classification
       end
 
       def add_count_for_class(count, classification)
@@ -104,9 +131,13 @@ module Basset
         @classes.values.sum
       end
       
+      def ==(other)
+        other.kind_of?(FeatureCount) && other.classes == @classes && other.name == @name
+      end
+      
       def inspect(opts={:verbose=>false})
         return super if opts[:verbose]
-        @feature_name.to_s + " => " + @classes.inspect
+        " ``" + @name.to_s + "''" + " --> " + @classes.inspect
       end
       
     end
---
+++ spec/unit/naive_bayes_spec.rb
@@ -0,0 +1,103 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe NaiveBayes::FeatureCount do
+  
+  it "should be equal to another feature count if the feature name and counts per class are equal" do
+    NaiveBayes::FeatureCount.new("rspec", :sweet, 1).should == NaiveBayes::FeatureCount.new("rspec", :sweet, 1)
+  end
+  
+end
+
+describe NaiveBayes do
+  
+  before(:each) do
+    @nbayes = NaiveBayes.new
+    @doc = Document.new("here are some interesting words", :interesting)
+    @feature_vectors = @doc.feature_vectors
+    @other_vectors = Document.new("these words are interesting", :interesting).feature_vectors
+    @test_vectors = Document.new("this word seems interesting", :interesting).feature_vectors
+  end
+  
+  def feature_counts_for(classification, *feature_count_tuples)
+    feature_counts = {}
+    feature_count_tuples.each do |tuple| 
+      feature_counts[tuple.first] = NaiveBayes::FeatureCount.new(tuple.first, classification, tuple.last)
+    end
+    feature_counts
+  end
+  
+  def add_interesting_docs
+    @nbayes.add_document(:interesting, @feature_vectors)
+    @nbayes.add_document(:interesting, @other_vectors)
+  end
+  
+  def add_boring_docs
+    @nbayes.add_document(:boring, Document.new("yawn lets go flame").feature_vectors)
+    @nbayes.add_document(:boring, Document.new("yawn lets flame and troll").feature_vectors)
+  end
+  
+  it "should keep track of the total docs and total docs for class when adding new docs" do
+    @nbayes.add_document(:interesting, @feature_vectors)
+    @nbayes.total_docs.should == 1
+    @nbayes.total_docs_in_class[:interesting].should == 1
+  end
+  
+  it "should create a feature count for each feature with the # of occurances and class" do
+    @nbayes.add_document(:interesting, @feature_vectors)
+    expected = feature_counts_for(:interesting, ["here", 1], ["ar", 1],["some", 1],["interest", 1],["word", 1])
+    @nbayes.feature_counts.should == expected
+  end
+  
+  it "should sum the number of all occurances of all features for a given class" do
+    @nbayes.add_document(:interesting, @feature_vectors)
+    @nbayes.occurrences_of_all_features_in_class(:intersting).should == 5
+  end
+  
+  it "should give a list of classes it knows about" do
+    @nbayes.add_document(:interesting, @feature_vectors)
+    @nbayes.add_document(:kinda_interesting, @feature_vectors)
+    @nbayes.classes.should == [:kinda_interesting, :interesting]
+  end
+  
+  it "should compute the probability that a given (singular) feature vector belongs to a given class" do
+    @nbayes.add_document(:interesting, @feature_vectors)
+    probablity = @nbayes.probability_of_vector_for_class(@test_vectors.first, :interesting)
+    probablity.should be_a Float
+    probablity.round.should == -1
+  end
+  
+  it "should compute the probability that a given set of feature vectors belongs to a given class" do
+    add_interesting_docs
+    probability = @nbayes.probability_of_vectors_for_class(@test_vectors, :interesting)
+    probability.should be_a(Float)
+    probability.round.should == -7
+  end
+  
+  it "should compute a probability of a class for a set of vectors normalized by the number of features" do
+    add_interesting_docs
+    probability = @nbayes.probability_of_vectors_for_class(@test_vectors, :interesting, :normalize => true)
+    probability.should be_a Float
+    probability.round.should == -2
+  end
+  
+  it "should determine the most likely class of a set feature vectors" do
+    add_interesting_docs
+    add_boring_docs
+    test_vectors = Document.new("some interesting words").feature_vectors
+    classification = @nbayes.classify(test_vectors, :normalize_classes => false)
+    classification.last.should == :interesting
+    classification.first.should be_a Float
+    classification.first.round.should == -4
+  end
+  
+  it "should account for the relative probabilities of classes by default when classifying" do
+    add_interesting_docs
+    add_boring_docs
+    vectors = Document.new("some interesting words").feature_vectors
+    classification = @nbayes.classify(vectors)
+    classification.last.should == :interesting
+    classification.first.should be_a Float
+    classification.first.round.should == -4
+  end
+  
+end
